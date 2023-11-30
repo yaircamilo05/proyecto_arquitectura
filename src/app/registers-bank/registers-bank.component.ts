81da7219788d3c1cbced7f92a25ce9f84ee9639e
disto-stack <juan.ocampo.2501@gmail.com>
2023-11-30 14:32:56 -0500

    feat(app): add registers bank component

---
+++ src/app/registers-bank/registers-bank.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  WritableSignal,
+  signal,
+} from '@angular/core';
+import { CpuComponentComponent } from '../cpu-component/cpu-component.component';
+import { CpuRunnerService } from '../services/cpu-runner.service';
+import { RegistersBankService } from '../services/registers-bank.service';
+
+@Component({
+  selector: 'app-registers-bank',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './registers-bank.component.html',
+  styleUrl: './registers-bank.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class RegistersBankComponent extends CpuComponentComponent {
+  readonly registers = this.registersBankService.registersSignal.asReadonly();
+
+  constructor(
+    cpuRunnerService: CpuRunnerService,
+    private registersBankService: RegistersBankService
+  ) {
+    super(cpuRunnerService);
+
+    this.identifier = 'REGISTERS-BANK';
+  }
+
+  getBankQuantity(key: string) {
+    return this.registers()[key];
+  }
+}
---
+++ src/app/services/registers-bank.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, WritableSignal, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RegistersBankService {
+  registersSignal: WritableSignal<{ [key: string]: number }>;
+
+  constructor() {
+    const initialRegisters = {
+      Ax: 0,
+      Bx: 0,
+      Cx: 0,
+      Dx: 0,
+      Ex: 0,
+      Fx: 0,
+      Gx: 0,
+      Hx: 0,
+    };
+
+    this.registersSignal = signal(initialRegisters);
+  }
+
+  set registers(value: { [value: string]: number }) {
+    this.registersSignal.set(value);
+  }
+
+  addValueToRegister(key: string, value: number) {
+    const registers = this.registersSignal();
+
+    if (registers[key] !== null && registers[key] !== undefined) {
+      registers[key] = value;
+
+      this.registersSignal.set({ ...registers });
+    }
+  }
+
+  popRegisterValue(key: string) {
+    const registers = this.registersSignal();
+
+    if (registers[key] === null && registers[key] === undefined) {
+      return;
+    }
+
+    const value = registers[key];
+
+    registers[key] = 0;
+    this.registersSignal.set({ ...registers });
+
+    return value;
+  }
+}

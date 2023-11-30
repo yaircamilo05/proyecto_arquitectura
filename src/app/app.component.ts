34fe8759fb386d1fee6297ce18f8822dca7d544e
disto-stack <juan.ocampo.2501@gmail.com>
2023-11-30 10:19:28 -0500

    feat(app): add instructions input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,32 +2,46 @@ import { Component } from '@angular/core';
 import { CpuRunnerService } from './services/cpu-runner.service';
 import { CpuComponentType } from './types/cpu-component.type';
 import { PrincipalMemoryService } from './services/principal-memory.service';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
+interface InstructionsForm {
+  instructions: FormControl<string>;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  instructionsForm: FormGroup<InstructionsForm>;
+
   constructor(
     private cpuRunnerService: CpuRunnerService,
-    private principalMemoryService: PrincipalMemoryService
-  ) {}
+    private principalMemoryService: PrincipalMemoryService,
+    private fb: FormBuilder
+  ) {
+    this.instructionsForm = new FormGroup({
+      instructions: new FormControl('', {
+        nonNullable: true,
+        validators: [Validators.required],
+      }),
+    });
+  }
 
   selectCpuComponent(
     component: CpuComponentType,
     content?: string | number | string[]
-  ) {
-    if (content) {
-      this.cpuRunnerService.selectComponentWitContent(component, content);
-      return;
-    }
+  ) {}
 
-    this.cpuRunnerService.selectComponent(component);
-  }
+  executeInstructions() {
+    const instructionsValue = this.instructionsForm.controls.instructions.value;
+    const instructions = instructionsValue.split(',');
 
-  getLastElement() {
-    const element = this.principalMemoryService.firstInstruction;
-    console.log(element);
+    // Start here
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CpuRunnerComponent } from './cpu-runner/cpu-runner.component';
 
+import { ButtonModule } from 'primeng/button';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CpuRunnerComponent],
+  imports: [
+    BrowserModule,
+    ButtonModule,
+    ReactiveFormsModule,
+    InputTextareaModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    CpuRunnerComponent,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
